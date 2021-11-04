309b8af9ea25ec349fcc0b264bab6f84d943dfc1
Gianmarco Folchi <gmfolc@gmail.com>
2021-11-04 11:13:47 -0400

    Added Project 2

--- HR/src/Main.java
+++ HR/src/Main.java
@@ -2,57 +2,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Menu myMenu = new Menu();
-//
-//        char option;
-//        int id;
-//        String name;
-//        Scanner scanner = new Scanner(System.in);
-//        Company company = new Company();
-//
-//        do {
-//            option = myMenu.getChoice();
-//
-//            switch (option) {
-//                case 'l':
-//                    System.out.println(company.toString());
-//                    break;
-//
-//                case 'h':
-//                    System.out.println("Enter the id of the new Employee");
-//                    id = scanner.nextInt();
-//                    System.out.println("Enter the name of the employee");
-//                    scanner = new Scanner(System.in);
-//                    name = scanner.nextLine();
-//                    company.hire(new Employee(id, name));
-//                    break;
-//
-//                case 'f':
-//                    System.out.println("Enter the id of the employee to fire");
-//                    int index = scanner.nextInt();
-//                    if (company.fire(index)) {
-//                        System.out.println("Employee is fired");
-//                    } else {
-//                        System.out.println("Employee does not exist");
-//                    }
-//                    break;
-//
-//                case 's':
-//                    System.out.println("Enter the id of the employee");
-//                    int tempID = scanner.nextInt();
-//                    System.out.println(company.getEmployee(tempID));
-//
-//                    break;
-//
-//                case 'q':
-//                    System.out.println("quitting");
-//                    break;
-//
-//                default:
-//                    System.out.println("Invalid choice...Try again");
-//            }
-//
-//        } while (option != 'q');
+        Menu myMenu = new Menu();
+
+        char option;
+        int id;
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        Company company = new Company();
+
+        do {
+            option = myMenu.getChoice();
+
+            switch (option) {
+                case 'l':
+                    System.out.println(company.toString());
+                    break;
+
+                case 'h':
+                    System.out.println("Enter the id of the new Employee");
+                    id = scanner.nextInt();
+                    System.out.println("Enter the name of the employee");
+                    scanner = new Scanner(System.in);
+                    name = scanner.nextLine();
+                    company.hire(new Employee(id, name));
+                    break;
+
+                case 'f':
+                    System.out.println("Enter the id of the employee to fire");
+                    int index = scanner.nextInt();
+                    if (company.fire(index)) {
+                        System.out.println("Employee is fired");
+                    } else {
+                        System.out.println("Employee does not exist");
+                    }
+                    break;
+
+                case 's':
+                    System.out.println("Enter the id of the employee");
+                    int tempID = scanner.nextInt();
+                    System.out.println(company.getEmployee(tempID));
+
+                    break;
+
+                case 'q':
+                    System.out.println("quitting");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice...Try again");
+            }
+
+        } while (option != 'q');
     }
 }
 
